c22ea758dd254d62ee73f399a3144bb214600ab5
천림 <Rym@cheonlim-ui-MacBook-Pro.local>
2015-03-18 16:25:38 +0900

    Initial Commit

---
+++ DesignPatternLearner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPatternLearner
+//
+//  Created by 천림 on 2015. 3. 18..
+//  Copyright (c) 2015년 천림. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
